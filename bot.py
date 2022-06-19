c3be51781720c40145ba101121b0b30f9a89c904
Jonah Hirsch <jonahwh@gmail.com>
2022-06-19 15:23:54 -0700

    No time fractions

--- bot.py
+++ bot.py
@@ -312,7 +312,7 @@ class DiscordGSM():
             if image_url:
                 embed.set_thumbnail(url=image_url)
 
-            embed.set_footer(text=f'DiscordGSM v.{VERSION} | Game Server Monitor | {FIELD_LASTUPDATE}: <t:{time.time()}:R>{SPACER}', icon_url=CUSTOM_IMAGE_URL)
+            embed.set_footer(text=f'DiscordGSM v.{VERSION} | Game Server Monitor | {FIELD_LASTUPDATE}: <t:{int(time.time())}:R>{SPACER}', icon_url=CUSTOM_IMAGE_URL)
         except Exception as e:
             self.print_to_console(f'Error Building embed.\n{e}')
 
